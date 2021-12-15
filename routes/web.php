a2e29fa2cfccdd1d7ceb44d645cbede8ca388992
Fathania Zulfani <fathaniazulfanii@gmail.com>
2021-12-15 12:08:06 +0700

    p

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@ $router->get('/data', function () use ($router) {
     return response()->json($results);
 });
 
-$router->post('/register', 'UserController@register');
-$router->post('/login','AuthController@login');
+$router->post('api/register', 'UserController@register');
+$router->post('api/login','AuthController@login');
 
 $router->group(['middleware' => 'auth'], function() use ($router){
     $router->post('/logout', 'AuthController@logout');
