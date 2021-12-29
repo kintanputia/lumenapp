04357a66e96bbd32d94bdf2e5007a1bc182f3a57
Fathania Zulfani <fathaniazulfanii@gmail.com>
2021-12-29 19:28:26 +0700

    p

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,7 +27,6 @@ class UserController extends Controller
         // $user = User::create($request->all());
 
         $user = User::create([
-                    'id' => 1,
                     'email' => $request->email,
                     'password' => $request->password,
                     'nama' => $request->nama,
