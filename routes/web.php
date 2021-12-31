8c24713b7462a006d8680bb98c43c70ffb9ea42c
kintanputia <kintanputia090101@gmail.com>
2021-12-31 23:06:03 +0700

    dijwiw

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ $router->get('/', function () use ($router) {
     return ["Hello Hai..!!!"];
 });
 
-$router->post('searchPengajian', 'PengajianController@index');
+$router->post('cari_pengajian', 'PengajianController@index');
 
 $router->get('/pengajian', function () use ($router) {
     $results = app('db')->select("SELECT * FROM pengajian INNER JOIN masjid ON pengajian.id_masjid=masjid.id_masjid");
